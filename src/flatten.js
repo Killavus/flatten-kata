0ac480a04c8428dad95a03160dd16490a5167d11
Marcin Grzywaczewski <killavus@gmail.com>
2016-08-03 20:00:33 +0200

    Written an implementation passing all tests.
    I've used the popular pattern of 'list folding' which is popular in
    functional environments. I'm trying to avoid using explicit iteration
    patterns (like `for ... of` loops) in favor of functional iteration
    primitives.

--- src/flatten.js
+++ src/flatten.js
@@ -1,5 +1,12 @@
 function flatten(source) {
-  return source;
+  return source.reduce(function flattenReducer(partialResult, element) {
+    if (Array.isArray(element)) {
+      const nestedArrayResult = flatten(element);
+      return [...partialResult, ...nestedArrayResult];
+    }
+
+    return [...partialResult, element];
+  }, []);
 }
 
 module.exports = flatten;
