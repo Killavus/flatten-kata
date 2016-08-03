b02551bd29e91643e8fff64331b33ba8af91ffb4
Marcin Grzywaczewski <killavus@gmail.com>
2016-08-03 20:14:13 +0200

    Extracted function for more intention-revealing code.
    The step with a nested array is not totally intention-revealing. I've
    decided to extract this behaviour to a function and give it a more sensible name.

--- src/flatten.js
+++ src/flatten.js
@@ -1,8 +1,12 @@
+function concatenateFlattened(flattenedArray, nestedArray) {
+  const flattenedNestedArray = flatten(nestedArray);
+  return [...flattenedArray, ...flattenedNestedArray];
+}
+
 function flatten(source) {
   return source.reduce(function flattenReducer(partialResult, element) {
     if (Array.isArray(element)) {
-      const nestedArrayResult = flatten(element);
-      return [...partialResult, ...nestedArrayResult];
+      return concatenateFlattened(partialResult, element);
     }
 
     return [...partialResult, element];
