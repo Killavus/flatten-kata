089541fe5293964220575f324996d3e92f28db90
Marcin Grzywaczewski <killavus@gmail.com>
2016-08-03 19:50:55 +0200

    Flat array test case.
    This forces me to remove the duplication from test & code.

--- src/flatten.test.js
+++ src/flatten.test.js
@@ -8,3 +8,11 @@ test('flattening an empty array results in an empty array', function(testCase) {
   const expectedResult = [];
   testCase.deepEqual(flatten(input), expectedResult);
 });
+
+test('flattening a flat array results in the same array', function(testCase) {
+  testCase.plan(1);
+  const input = [1, 2, 3];
+
+  const expectedResult = [1, 2, 3];
+  testCase.deepEqual(flatten(input), expectedResult);
+});
