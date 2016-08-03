c53f7c4ce5c53383aeb1a9617db22963cad04432
Marcin Grzywaczewski <killavus@gmail.com>
2016-08-03 20:09:36 +0200

    Added a test for deeply nested arrays as inputs.
    It seems that the current implementation is strong enough to handle
    flattening deeply nested arrays. That being said, the solution is
    fully functional now.

--- src/flatten.test.js
+++ src/flatten.test.js
@@ -30,7 +30,27 @@ test('shallowly nested arrays are flattened', function(testCase) {
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [1, 2, 3, 4, 5, 6, 7],
-    []
+    [],
+  ];
+
+  inputs.forEach(function(input, index) {
+    testCase.deepEqual(flatten(input), expectedResults[index]);
+  });
+});
+
+test('deeply nested arrays are flattened', function(testCase) {
+  const inputs = [
+    [[[[1]]], [2, [3, [4, 5]]], [6, [[7]]], [8], 9, 10],
+    [[1, 2, 3, [4, 5, [[[[[6, 7]]]]]]]],
+    [[[[[[[[[]]]]]]]], [1], [2, 3], [[4, 5], 6, [7, [8, [9]]], [[[10]]], 11]],
+  ];
+
+  testCase.plan(inputs.length);
+
+  const expectedResults = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2, 3, 4, 5, 6, 7],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
   ];
 
   inputs.forEach(function(input, index) {
